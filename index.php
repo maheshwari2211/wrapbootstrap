74311505c4c0e2f7ba454dd1b8b6e0848d03031f
maheshwari2211 <37235461+maheshwari2211@users.noreply.github.com>
2018-03-15 10:04:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>wrapbootsrap</title>
+	<link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css" />
+	<link href="css/style1.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div>
+	<div class="wrapper">
+	<div class="sticky">
+		<header>
+			<div style="width:80%;margin:0 auto">
+				<div><p class="topbar_one" >{Wrap} bootstrap</p></div>
+				<div><p class="topbar_two"> Minor-Responsive Website Template</p></div>
+				<div class="topbar_three"><button>remove this frame>></button></div>
+			</div>
+		</header>
+		<nav><!--nav start-->
+			<div class="wrap">
+				<div class="nav_one"><p>Miro<span style="color:red">n:</span></p>
+				</div>
+				<div class="nav_two">
+				<div>
+					<ul>
+					
+					<li><a class="home" href="#">HOME <span class="fa fa-angle-down ">
+					
+			</span></a></li>
+					<li><a class="home1" href="#">PORTFOLIO <span class="fa fa-angle-down"></span></a></li>
+					<li><a class="home1" href="#">BLOG <span class="fa fa-angle-down"></span></a></li>
+					<li><a class="home1" href="#">FEATURES <span class="fa fa-angle-down"></span></a></li>
+					<li><a class="home1" href="#">PAGES <span class="fa fa-angle-down"></span></a></li>
+					</ul>
+				</div>
+				</div>
+			</div>
+		</nav><!--nav end-->
+		</div>
+		<section><!--artical1 start-->
+		
+			<div>
+				<img class="relative" src="images/building.jpg" alt="build"/>
+			</div>
+			<div class="absolute">
+				<h1>Build with Bootstarp3</h1>
+				<p class="abstxt">Miron is a multipurpose theme that you can use for your business Portfolio or blog.</p><br><br><br>
+				<a class="abstxt1" href="#"><span class="abstxt">Learn More</span></a>
+			</div>
+		</section><!--artical1 end-->
+									<!--artical2 start-->
+		<div class="artical2">
+			<h1 class="artical2_one">Miron  HTML5  CSS3  fully  Responsive  website  template</h1>
+			<h2 class="artical2_two"> with clean valid code </h2>
+		</div><!--artical2 end-->
+		
+		<div class="artical3"><!--artical3 start-->
+			<div class="wrap">
+			<div class="artical3_one">
+				<div><span class="fa fa-heart-o" style="color:red;font-size:30px;text-align:center;margin-left:128px"></span></div>
+				<p class="art3"> Make with Love</p>
+				<div class="art3_one" ><p>Pellentesque in tempor metus. Vestibulum sit amet elit justo, vitae gravida erat. Nam hendrerit leo non enim consectetur tristique eleifend nec turpis. Nulla fermentum, ante<br> dapibus</p></div>
+			</div>
+			<div  class="artical3_one">
+				<div><span class="fa fa-twitter" style="color:red;font-size:30px;text-align:center;margin-left:128px"></span></div>
+				<p class="art3">Bootstrap3</p>
+				<div class="art3_one"><p>Pellentesque in tempor metus. Vestibulum sit amet elit justo, vitae gravida erat. Nam hendrerit leo non enim consectetur tristique eleifend nec turpis. Nulla fermentum, ante<br> dapibus</p></div>
+			</div>
+			<div  class="artical3_one">
+				<div><span style="color:red;font-size:27px;margin-left:128px" >{}</span></div>
+				<p class="art3"> Clean</p>
+				<div class="art3_one"><p>Pellentesque in tempor metus. Vestibulum sit amet elit justo, vitae gravida erat. Nam hendrerit leo non enim consectetur tristique eleifend nec turpis. Nulla fermentum, ante<br> dapibus</p></div>
+			</div>
+			<div  class="artical3_one">
+				<div><span class="fa fa-desktop" style="color:red;font-size:30px;text-align:center;margin-left:128px"></span></div>
+				<p class="art3"> Fully responsive</p>
+				<div class="art3_one" ><p>Pellentesque in tempor metus. Vestibulum sit amet elit justo, vitae gravida erat. Nam hendrerit leo non enim consectetur tristique eleifend nec turpis. Nulla fermentum, ante<br> dapibus</p></div>
+			</div>
+			</div>
+		</div><!--artical3 end-->
+		<div class="clearfix"></div>
+		<div class="artical4"><!--artical4 start-->
+			<div>
+				<div class=""><h1>Recently Work</h1></div>
+				<span class="fa fa-picture-o" style="color:red;font-size:30px;text-align:center;margin-left:650px;padding-top:10px"></span>
+			</div>
+			
+			<div class="nav_t">
+				<ul>
+				<li  class="lil"><a href="#">all</a></li>
+				<li class="lil1"><a href="#">design</a></li>
+				<li class="lil1"><a href="#">html5</a></li>
+				<li class="lil1"><a href="#">wordpress</a></li>
+				<li class="lil1"><a href="#">seo</a></li>
+				</ul>
+			</div>
+		</div><!--artical4 end-->
+		<div class="marginlin "></div>
+		<div class="artical5"><!--artical5 start-->
+			<div class="wrap1">
+				<div class="art5_one">
+					<div class="art5"><img class="art5img" src="images/img-1.jpg" alt="img"/></div>
+					<div class="art5"><img class="art5img" src="images/img-2.jpg" alt="img"/></div>
+					<div class="art5"><img class="art5img" src="images/img-3.jpg" alt="img"/></div>
+				</div>
+				<div class="art5_one">
+					<div class="art5" ><img class="art5img" src="images/img-4.jpg" alt="img"/></div>
+					<div class="art5" ><img class="art5img" src="images/img-5.jpg" alt="img"/></div>
+					<div class="art5" ><img class="art5img" src="images/img-6.jpg" alt="img"/></div><br>	
+				</div>
+			</div>	
+		</div>
+		<!--artical5 end-->
+		<div class="clearfix"></div>
+		<div class="artical6"><!--artical6 start-->
+			<div>
+				<h1>About Us</h1><br>
+				<div><img class="people" src="images/people.jpg" alt="people"/></div>
+			</div>
+			<div class="wrap2">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pulvinar scelerisque dictum. Donec iaculis, diam sit amet suscipit feugiat, diam magna volutpat augue, sit amet varius nulla turpis sit amet lacus. Aenean faucibus, sem quis convallis lobortis. Aenean faucibus, lacus sem quis convallis lobortis.</p><br>
+			
+				<div class="logo"><span class="fa fa-picture-o fa-pull-left fa-border"style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-twitter fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-google-plus fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-wifi fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-linkedin-square fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-pinterest fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-skype fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-dribbble fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-yahoo fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				<span class="fa fa-github fa-pull-left fa-border" style="color:#040404;background-color:#fff;font-size:20px"></span>
+				</div>
+			</div>
+		</div><!--artical6 end-->
+		<br>
+		<div class="artical7"><!--artical7 start-->
+			<h1>Latest News</h1>
+			<span class="fa fa-pencil-square-o logo" style="color:red;font-size:30px;text-align:center;margin-left:650px;padding-top:10px"></span>
+		</div><!--artical7 end-->
+		<div class="artical8"><!--artical8 start-->
+			<div class="wrap3">
+			<div class="art8"><img class="art8img"src="images/img-1.jpg" alt="img"/><br>
+				<h3>Amazing theme for multi-purpose</h3>
+				<p>Aliqua adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+			</div>
+			<div class="art8"><img  class="art8img" src="images/img-6.jpg" alt="img"/><br>
+				<h3>Amazing theme for multi-purpose</h3>
+				<p>Aliqua adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+			</div>
+			<div class="art8"><img class="art8img" src="images/img-3.jpg" alt="img"/><br>
+				<h3>Amazing theme for multi-purpose</h3>
+				<p>Aliqua adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+			</div>
+			</div>
+		</div><!--artical8 end-->
+		<div class="clearfix"></div>
+		<div class="footer"><!--footer start-->
+		<div class="wrap1">
+			<div>
+			<div class="foot_one">
+				<h2>About Us</h2>
+				<p>Lorem ipsum ex vix illud nonummy, novum tation et his. At vix scriptaset patrioque scribentur, at pro fugit erts verterem molestiae, sed et vivendo ali Lorem ipsum ex vix illud nonummy, novum tation et his. At vix scripta patrioque scribentur.</p>
+			</div>
+			<div class="foot_one">
+				<h2>Latest Post</h2>
+				<div class="ft">
+				<p>Blog slide post</p><hr>
+				<p>Lorem ipsum dollor</p><hr>
+				<p>Blog image post</p><hr>
+				<p>Blog Video post</p><hr>
+				<p>Lorem ipsum dollor</p>
+				</div>
+			</div>
+			
+			<div class="foot_one">
+				<h2>Latest work</h2>
+				<div class="latest">
+					<div class="latest_one"><img  class="latest_img"src="images/img-1.jpg" alt="img"/>
+						<img class="latest_img" src="images/img-2.jpg" alt="img"/>
+						<img class="latest_img" src="images/img-3.jpg" alt="img"/>
+					</div>
+					<div class="latest_one"><img class="latest_img"src="images/img-4.jpg" alt="img"/>
+						<img  class="latest_img"src="images/img-5.jpg" alt="img"/>
+						<img  class="latest_img"src="images/img-6.jpg" alt="img"/>
+					</div>
+					<div class="latest_one"><img  class="latest_img"src="images/img-3.jpg" alt="img"/>
+						<img  class="latest_img"src="images/img-2.jpg" alt="img"/>
+						<img class="latest_img" src="images/img-5.jpg" alt="img"/>
+					</div>
+				</div>
+			</div>
+			<div class="foot_one">
+				<div>
+					<h2>Get in Touch</h2>
+					<span class="fa fa-home"style="color:red;margin:5px"></span><span class="foot">124,munnawali Jaipur,302012</span><br>
+					<span class="fa fa-phone"style="color:red;margin:5px"></span><span class="foot">Phone: +91 1234567890</span><br>
+					<span class="fa fa-envelope"style="color:red;margin:5px"></span><span class="foot">Email: mail@domain.com</span><br>
+					<span class="fa fa-dribbble"style="color:red;margin:5px"></span><span class="foot">Website: http://www.site.com</span>
+				</div>
+				<div>
+					<span class="fa fa-facebook fa-pull-left fa-border one"style="color:#040404;background-color:#fff;margin:4px;font-size:20px"></span>
+					<span class="fa fa-twitter fa-pull-left fa-border"style="color:#040404;background-color:#fff;margin:4px;font-size:20px"></span>
+					<span class="fa fa-google-plus fa-pull-left fa-border"style="color:#040404;background-color:#fff;margin:4px;font-size:20px"></span>
+					<span class="fa fa-pinterest fa-pull-left fa-border"style="color:#040404;background-color:#fff;margin:4px;font-size:20px"></span>
+					<span class="fa fa-skype fa-pull-left fa-border"style="border:#040404;background-color:#fff;margin:4px;font-size:22px"></span>
+				</div>
+			
+			</div>
+			</div>
+			<div class="clearfix"></div>
+			<footer>
+				<div class="copy_one"><p>Miro<span class="copy">n:<span></p></div>
+				<div class="copy_two"><p>© copyright 2014.All right reserved.</p></div>
+			</footer>
+		</div>
+		</div><!--footer end-->
+	</div><!--wrapper end-->
+
+</div>
+
+</body>
+</html>
